d181a19c65b427b8a8545b6f73a1322146c6fed7
Facundo Merighi <merighifacundo@gmail.com>
2023-05-23 11:52:04 -0300

    first commit

---
+++ src/ApplicationLuncher.java
@@ -0,0 +1,12 @@
+
+public class ApplicationLuncher {
+
+	public static void main(String[] args) {
+		// Use the facade design pattern to achieve:
+		// -> launchSpaceship to destination
+		// -> abortMission
+		
+
+	}
+
+}
---
+++ src/com/spacetrips/systems/PowerSystem.java
@@ -0,0 +1,11 @@
+package com.spacetrips.systems;
+
+public class PowerSystem {
+    public void powerOn() {
+        System.out.println("Power System: Powering on...");
+    }
+
+    public void powerOff() {
+        System.out.println("Power System: Powering off...");
+    }
+}
